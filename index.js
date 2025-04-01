ca224a19d9822305ef3f96189789552ee4a73c58
RazovP <baxikmc@gmail.com>
2025-04-01 23:45:51 +0300

    hw-4

---
+++ index.js
@@ -0,0 +1,6 @@
+import { renderComments } from "./modules/renderComments.js";
+import { newComment } from "./modules/initButton.js";
+
+renderComments();
+newComment();
+console.log("It works!");
---
+++ modules/comments.js
@@ -0,0 +1,4 @@
+export const comments = [
+  { name: "Глеб Фокин", date: "12.02.22 12:18", commentText: "Это будет первый комментарий на этой странице", likeCounter: 3, aktiveLike: false },
+  { name: "Варвара Н.", date: "13.02.22 19:22", commentText: "Мне нравится как оформлена эта страница! ❤", likeCounter: 75, aktiveLike: true }
+];
